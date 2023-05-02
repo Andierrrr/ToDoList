95e6ff9ed7eb6a1da52e774927163b46221c0ca9
andierrrr <allaican@kean.edu>
2023-05-02 16:38:05 -0400

    TDList

---
+++ TodoList/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace ToDoList.Models
+{
+    public class Todo
+    {
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Category { get; set; }
+    }
+}
+
